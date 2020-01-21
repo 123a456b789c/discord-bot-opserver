b3ab43ec1fbd59cb61f8b14a70d207195f466a96
123a456b789c <50078004+123a456b789c@users.noreply.github.com>
2020-01-21 17:09:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,222 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create the Discord client
+const client = new Discord.Client();
+
+// When the app is ready it will write out Ready using this
+client.once('ready', () => {
+	console.log('ELINDITVA');
+	console.log('CHEAT PROTECTION: ONLINE')
+});
+
+client.on('message', message => {
+	if (message.content === '!Hello') {
+        // send back "Pong." to the channel the message was sent in
+	console.log('#valaki koszont');
+        message.channel.send('Cso');
+	}
+	if (message.content === '!parancsok') {
+	console.log('#[parancsok lekérve]')
+	message.channel.send(' !Hello \n !teszt \n !parancsok')
+	}
+	if (message.content === '!teszt') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('TESZT: SIKERES');
+        }
+	if (message.content == 'Anyád') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'anyád') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'anyad') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Anyad') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'kurva') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Kurva') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'kurva anyad') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'kurva Anyad') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'kurva anyád') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'kurva Anyád') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'buzi') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Buzi') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'pocsjanko') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'pöcsjankó') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'faszjankó') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'faszjanko') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'fasz') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Fasz') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'bazdmeg') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Bazdmeg') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Köcsög') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'kocsog') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Kocsog') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'köcsög') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'pénisz') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'buzi') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Buzi') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Búzi') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'búzi') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'zsidó') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'zsido') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Zsidó') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content == 'Zsido') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+        }
+	if (message.content.startsWith('zsi')) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+	console.log('[INTELLIGENT-ANTI-CHEAT ALERT]')
+        }
+	if (message.content.endsWith('do')) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+	console.log('[INTELLIGENT-ANTI-CHEAT ALERT]')
+        }
+	if (message.content.startsWith('Zsi')) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+	console.log('[INTELLIGENT-ANTI-CHEAT ALERT]')
+        }
+	if (message.content.endsWith('DO')) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+	console.log('[INTELLIGENT-ANTI-CHEAT ALERT]')
+        }
+	if (message.content.startsWith('Any')) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+	console.log('[INTELLIGENT-ANTI-CHEAT ALERT]')
+        }
+	if (message.content.endsWith('AD')) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+	console.log('[INTELLIGENT-ANTI-CHEAT ALERT]')
+        }
+	if (message.content.startsWith('ANY')) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+	console.log('[INTELLIGENT-ANTI-CHEAT ALERT]')
+        }
+	if (message.content.endsWith('ad')) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+	console.log('[INTELLIGENT-ANTI-CHEAT ALERT]')
+        }
+	if (message.content.startsWith('fas')) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+	console.log('[INTELLIGENT-ANTI-CHEAT ALERT]')
+        }
+	if (message.content.startsWith('FAS')) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('nana figyelünk');
+	console.log('[INTELLIGENT-ANTI-CHEAT ALERT]')
+        }
+	
+
+
+
+
+
+
+
+});
+
+
+// Replace the value between the quotes with your token
+client.login('NjY3NDIzNTI0OTYwMzM3OTQ0.XiCgog.JxBxkY16qsRQQPgn_7rCyGN-JTE');
+
